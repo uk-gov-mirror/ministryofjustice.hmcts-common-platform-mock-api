625783e54370b3e3a268f96fab63ad13ea1e2e0d
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-21 17:26:11 +0100

    Rubocop fix

--- spec/support/schemas/parse_script.rb
+++ spec/support/schemas/parse_script.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'json'
 
 # run like ruby parse_script <name_of_json_schema.json>
@@ -8,16 +10,15 @@ data_hash = JSON.parse(file)
 
 generator_string = "rails g model #{ARGV[0][0].upcase}#{ARGV[0][1..].gsub('.json', '')} "
 
-
 data_hash['properties'].each do |property|
   name = property[0]
-  if property[1]['type']
-    generator_string << "#{name}:#{property[1]['type']} "
-  elsif property[1]['$ref'].include?('uuid')
-    generator_string << "#{name}:uuid "
-  else
-    generator_string << "#{name}:references "
-  end
+  generator_string << if property[1]['type']
+                        "#{name}:#{property[1]['type']} "
+                      elsif property[1]['$ref'].include?('uuid')
+                        "#{name}:uuid "
+                      else
+                        "#{name}:references "
+                      end
 end
 
 puts generator_string
