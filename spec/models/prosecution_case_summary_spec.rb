fc825dffbb035225401158e25584326be145fef0
Arlston Fernandes <gpg@arlston.com>
2020-03-30 16:42:24 +0100

    Validate Search and Hearing schemas

--- spec/models/prosecution_case_summary_spec.rb
+++ spec/models/prosecution_case_summary_spec.rb
@@ -10,6 +10,33 @@ RSpec.describe ProsecutionCaseSummary, type: :model do
 
   it_has_behaviour 'conforming to valid schema'
 
+  fcontext 'search_prosecution_case_response' do
+    let(:json_schema) { :search_prosecution_case_response }
+    let(:body) { JSON.parse(File.open(Rails.root.join('search.json')).read) }
+
+    it 'matches the given schema' do
+      expect(body).to match_json_schema(json_schema)
+    end
+  end
+
+  fcontext 'results_hearing_resulted_response' do
+    let(:json_schema) { :results_hearing_resulted_response }
+    let(:body) { JSON.parse(File.open(Rails.root.join('hearing.json')).read) }
+
+    it 'matches the given schema' do
+      expect(body).to match_json_schema(json_schema)
+    end
+  end
+
+  fcontext 'hearing' do
+    let(:json_schema) { :hearing }
+    let(:body) { JSON.parse(File.open(Rails.root.join('hearing.json')).read) }
+
+    it 'matches the given schema' do
+      expect(body).to match_json_schema(json_schema)
+    end
+  end
+
   context 'with a hearing relationship' do
     before do
       prosecution_case.hearing = FactoryBot.create(:hearing)
